2e4e14fe1210ade8a2c70ff02b04b8970198d553
Rodrigo Solis <solisrodrigo619@gmail.com>
2023-07-19 16:55:23 -0300

    Entityframework commit

---
+++ APISistemaVentas/SistemaVenta.Model/DetalleVentum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaVenta.Model;
+
+public partial class DetalleVentum
+{
+    public int IdDetalleVenta { get; set; }
+
+    public int? IdVenta { get; set; }
+
+    public int? IdProducto { get; set; }
+
+    public int? Cantidad { get; set; }
+
+    public decimal? Precio { get; set; }
+
+    public decimal? Total { get; set; }
+
+    public virtual Producto? IdProductoNavigation { get; set; }
+
+    public virtual Ventum? IdVentaNavigation { get; set; }
+}
---
+++ APISistemaVentas/SistemaVenta.Model/Ventum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaVenta.Model;
+
+public partial class Ventum
+{
+    public int IdVenta { get; set; }
+
+    public string? NumeroDocumento { get; set; }
+
+    public string? TipoPago { get; set; }
+
+    public decimal? Total { get; set; }
+
+    public DateTime? FechaRegistro { get; set; }
+
+    public virtual ICollection<DetalleVentum> DetalleVenta { get; } = new List<DetalleVentum>();
+}

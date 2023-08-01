561f140c7b6c621e8f5182fe79300b0c3ee4a1ff
Rodrigo Solis <solisrodrigo619@gmail.com>
2023-07-31 22:18:04 -0300

    update user

--- APISistemaVentas/SistemaVenta.BLL/Servicios/Contrato/IUsuarioService.cs
+++ APISistemaVentas/SistemaVenta.BLL/Servicios/Contrato/IUsuarioService.cs
@@ -17,6 +17,6 @@ namespace SistemaVenta.BLL.Servicios.Contrato
         Task<SesionDTO> ValidarCredenciales(string correo, string clave);
         Task<UsuarioDTO> Crear(UsuarioDTO modelo);
         Task<bool> Editar(UsuarioDTO modelo);
-        Task<bool> Eliominar (int id);
+        Task<bool> Eliminar (int id);
     }
 }
--- APISistemaVentas/SistemaVenta.DAL/Repositorios/Contratos/IGenericRepository.cs
+++ APISistemaVentas/SistemaVenta.DAL/Repositorios/Contratos/IGenericRepository.cs
@@ -21,7 +21,10 @@ namespace SistemaVenta.DAL.Repositorios.Contratos
         //ojo aca porque antes era 
         //Task<IQueryable<TModel>> Consultar(Expression<Func<TModel, bool>> filtro); pero le saque el filtro porque me tiraba error 
         //en  var listaRoles = await _rolRepositorio.Consultar();
+        //Task<IQueryable<TModel>> Consultar();
+        Task<IQueryable<TModel>> Consultar(Expression<Func<TModel, bool>> filtro);
         Task<IQueryable<TModel>> Consultar();
+
 
     }
 }

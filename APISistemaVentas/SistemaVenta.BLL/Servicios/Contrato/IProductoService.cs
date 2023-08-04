da2492bce34be6f8196400d957c24775bb6cacbe
Rodrigo Solis <solisrodrigo619@gmail.com>
2023-08-04 03:39:04 -0300

    update  services

---
+++ APISistemaVentas/SistemaVenta.BLL/Servicios/Contrato/IProductoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DTO;
+
+namespace SistemaVenta.BLL.Servicios.Contrato
+{
+    public interface IProductoService
+    {
+        Task<List<ProductoDTO>> Lista();
+        Task<ProductoDTO> Crear(UsuarioDTO modelo);
+        Task<bool> Editar(ProductoDTO modelo);
+        Task<bool> Eliminar(int id);
+    }
+}

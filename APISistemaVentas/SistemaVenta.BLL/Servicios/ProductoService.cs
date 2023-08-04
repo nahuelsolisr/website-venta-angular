9f8b1b1bf0f57f87a5b3270d59b1a18bd35d413d
Rodrigo Solis <solisrodrigo619@gmail.com>
2023-08-04 03:45:56 -0300

    update

--- APISistemaVentas/SistemaVenta.BLL/Servicios/ProductoService.cs
+++ APISistemaVentas/SistemaVenta.BLL/Servicios/ProductoService.cs
@@ -4,9 +4,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DAL.Repositorios.Contratos;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+
 namespace SistemaVenta.BLL.Servicios
 {
-    internal class ProductoService
+    public class ProductoService : IProductoService
     {
+
+        private readonly IGenericRepository<Producto> _productoRepositorio;
+        private readonly IMapper _mapper;
+
+        public ProductoService(IGenericRepository<Producto> productoRepositorio, IMapper mapper)
+        {
+            _productoRepositorio = productoRepositorio;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ProductoDTO>> Lista()
+        {
+            try
+            {
+                var queryProducto = await _productoRepositorio.Consultar();
+                var listaProductos = queryProducto.Include(cat => cat.IdCategoriaNavigation).ToList();
+
+                return _mapper.Map<List<ProductoDTO>>(listaProductos.ToList());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<ProductoDTO> Crear(UsuarioDTO modelo)
+        {
+            try
+            {
+                var productoCreado = await _productoRepositorio.Crear(_mapper.Map<Producto>(modelo));
+
+                if (productoCreado.IdProducto == 0)
+                {
+                    throw new TaskCanceledException("No se pudo crear");
+                }
+
+                return _mapper.Map<ProductoDTO>(productoCreado);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Task<bool> Editar(ProductoDTO modelo)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Task<bool> Eliminar(int id)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }    
     }
 }
